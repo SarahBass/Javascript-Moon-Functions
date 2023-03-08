a56865eb7cf17a0ee57519724752cb47119811bb
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2023-03-08 15:06:43 -0800

    Create almanacmoon.js

---
+++ almanacmoon.js
@@ -0,0 +1,30 @@
+function getAlmanac() {
+
+      if (month == 0) {
+        return "Wolf Moon";
+      } else if (month == 1) {
+        return "Snow Moon";
+      } else if (month == 2) {
+        return "Worm Moon";
+      } else if (month == 3) {
+        return " Pink Moon";
+      } else if (month == 4) {
+        return "Flower Moon";
+      } else if (month == 5) {
+        return "Strawberry Moon";
+      } else if (month == 6) {
+        return "Buck Moon";
+      } else if (month == 7) {
+        return "Sturgeon Moon";
+      } else if (month == 8) {
+        return "Corn Moon";
+      } else if (month == 9) {
+        return "Harvest Moon";
+      } else if (month == 10) {
+        return "Beaver Moon";
+      } else if (month == 11) {
+        return "Cold Moon";
+      } else {
+        return "Moon";
+      }
+    }
