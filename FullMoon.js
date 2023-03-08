8ae0e7e5d8880d32bb25e5090f01329a15660b77
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2023-03-08 15:03:02 -0800

    Create FullMoon.js

---
+++ FullMoon.js
@@ -0,0 +1,112 @@
+function getFullMoonDate() {
+   
+    if(year ==          2022 && month ==  January  ){return  17 ;}
+      else if(year ==   2022 && month ==  February  ){return  16 ;}
+   else if(year ==      2022 && month ==  March  ){return  18 ;}
+   else if(year ==      2022 && month ==  April  ){return  16 ;}
+ else if(year ==        2022 && month ==  May  ){return  16 ;}
+  else if(year ==       2022 && month ==  June  ){return  14 ;}
+  else if(year ==       2022 && month ==  July  ){return  13 ;}
+    else if(year ==     2022 && month ==  August  ){return  11 ;}
+       else if(year ==  2022 && month ==  September  ){return  10 ;}
+    else if(year ==     2022 && month ==  October ){ return  9 ;}
+     else if(year ==    2022 && month ==  November ){ return  8 ;}
+     else if(year ==    2022 && month ==  December ){ return  7 ;}
+    else if(year ==     2023 && month ==  January ){ return  6 ;}
+     else if(year ==    2023 && month ==  February ){ return  5 ;}
+  else if(year ==       2023 && month ==  March ){ return  7 ;}
+  else if(year ==       2023 && month ==  April ){ return  5 ;}
+else if(year ==         2023 && month ==  May ){ return  5 ;}
+ else if(year ==        2023 && month ==  June ){ return  3 ;}
+ else if(year ==        2023 && month ==  July ){ return  3 ;}
+   else if(year ==      2023 && month ==  August ){ return  1 ;}
+       else if(year ==  2023 && month ==  September  ){return  29 ;}
+     else if(year ==    2023 && month ==  October  ){return  28 ;}
+      else if(year ==   2023 && month ==  November  ){return  27 ;}
+      else if(year ==   2023 && month ==  December  ){return  26 ;}
+     else if(year ==    2024 && month ==  January  ){return  25 ;}
+      else if(year ==   2024 && month ==  February  ){return  24 ;}
+   else if(year ==      2024 && month ==  March  ){return  25 ;}
+   else if(year ==      2024 && month ==  April  ){return  23 ;}
+ else if(year ==        2024 && month ==  May  ){return  23 ;}
+  else if(year ==       2024 && month ==  June  ){return  21 ;}
+  else if(year ==       2024 && month ==  July  ){return  21 ;}
+    else if(year ==     2024 && month ==  August  ){return  19 ;}
+       else if(year ==  2024 && month ==  September  ){return  17 ;}
+     else if(year ==    2024 && month ==  October  ){return  17 ;}
+      else if(year ==   2024 && month ==  November  ){return  15 ;}
+      else if(year ==   2024 && month ==  December  ){return  15 ;}
+     else if(year ==    2025 && month ==  January  ){return  13 ;}
+      else if(year ==   2025 && month ==  February  ){return  12 ;}
+   else if(year ==      2025 && month ==  March  ){return  13 ;}
+   else if(year ==      2025 && month ==  April  ){return  12 ;}
+ else if(year ==        2025 && month ==  May  ){return  12 ;}
+  else if(year ==       2025 && month ==  June  ){return  11 ;}
+  else if(year ==       2025 && month ==  July  ){return  10 ;}
+   else if(year ==      2025 && month ==  August ){ return  9 ;}
+      else if(year ==   2025 && month ==  September ){ return  7 ;}
+    else if(year ==     2025 && month ==  October ){ return  6 ;}
+     else if(year ==    2025 && month ==  November ){ return  5 ;}
+     else if(year ==    2025 && month ==  December ){ return  4 ;}
+    else if(year ==     2026 && month ==  January ){ return  3 ;}
+     else if(year ==    2026 && month ==  February ){ return  1 ;}
+  else if(year ==       2026 && month ==  March ){ return  3 ;}
+  else if(year ==       2026 && month ==  April ){ return  1 ;}
+else if(year ==         2026 && month ==  May ){ return  1 ;}
+  else if(year ==       2026 && month ==  June  ){return  29 ;}
+  else if(year ==       2026 && month ==  July  ){return  29 ;}
+    else if(year ==     2026 && month ==  August  ){return  27 ;}
+       else if(year ==  2026 && month ==  September  ){return  26 ;}
+     else if(year ==    2026 && month ==  October  ){return  25 ;}
+      else if(year ==   2026 && month ==  November  ){return  24 ;}
+      else if(year ==   2026 && month ==  December  ){return  23 ;}
+     else if(year ==    2027 && month ==  January  ){return  22 ;}
+      else if(year ==   2027 && month ==  February  ){return  20 ;}
+   else if(year ==      2027 && month ==  March  ){return  22 ;}
+   else if(year ==      2027 && month ==  April  ){return  20 ;}
+ else if(year ==        2027 && month ==  May  ){return  20 ;}
+  else if(year ==       2027 && month ==  June  ){return  18 ;}
+  else if(year ==       2027 && month ==  July  ){return  18 ;}
+    else if(year ==     2027 && month ==  August  ){return  17 ;}
+       else if(year ==  2027 && month ==  September  ){return  15 ;}
+     else if(year ==    2027 && month ==  October  ){return  15 ;}
+      else if(year ==   2027 && month ==  November  ){return  13 ;}
+      else if(year ==   2027 && month ==  December  ){return  13 ;}
+     else if(year ==    2028 && month ==  January  ){return  11 ;}
+      else if(year ==   2028 && month ==  February  ){return  10 ;}
+   else if(year ==      2028 && month ==  March  ){return  10 ;}
+  else if(year ==       2028 && month ==  April ){ return  9 ;}
+else if(year ==         2028 && month ==  May ){ return  8 ;}
+ else if(year ==        2028 && month ==  June ){ return  6 ;}
+ else if(year ==        2028 && month ==  July ){ return  6 ;}
+   else if(year ==      2028 && month ==  August ){ return  5 ;}
+      else if(year ==   2028 && month ==  September ){ return  3 ;}
+    else if(year ==     2028 && month ==  October ){ return  3 ;}
+     else if(year ==    2028 && month ==  November ){ return  2 ;}
+     else if(year ==    2028 && month ==  December ){ return  1 ;}
+     else if(year ==    2029 && month ==  January  ){return  29 ;}
+      else if(year ==   2029 && month ==  February  ){return  28 ;}
+   else if(year ==      2029 && month ==  March  ){return  29 ;}
+   else if(year ==      2029 && month ==  April  ){return  28 ;}
+ else if(year ==        2029 && month ==  May  ){return  27 ;}
+  else if(year ==       2029 && month ==  June  ){return  25 ;}
+  else if(year ==       2029 && month ==  July  ){return  25 ;}
+    else if(year ==     2029 && month ==  August  ){return  23 ;}
+       else if(year ==  2029 && month ==  September  ){return  22 ;}
+     else if(year ==    2029 && month ==  October  ){return  22 ;}
+      else if(year ==   2029 && month ==  November  ){return  20 ;}
+      else if(year ==   2029 && month ==  December  ){return  20 ;}
+     else if(year ==    2030 && month ==  January  ){return  19 ;}
+      else if(year ==   2030 && month ==  February  ){return  17 ;}
+   else if(year ==      2030 && month ==  March  ){return  19 ;}
+   else if(year ==      2030 && month ==  April  ){return  17 ;}
+ else if(year ==        2030 && month ==  May  ){return  15 ;}
+  else if(year ==       2030 && month ==  June  ){return  14 ;}
+  else if(year ==       2030 && month ==  July  ){return  13 ;}
+    else if(year ==     2030 && month ==  August  ){return  11 ;}
+       else if(year ==  2030 && month ==  September  ){return  11 ;}
+     else if(year ==    2030 && month ==  October  ){return  11 ;}
+     else if(year ==    2030 && month ==  November ){ return  9 ;}
+     else if(year ==    2030 && month ==  December ){ return  9 ;}
+       else {return 1;}
+}
