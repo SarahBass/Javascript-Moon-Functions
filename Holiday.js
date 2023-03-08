27a79c248229fd75b79a513a9d43c6ab88f1c5ca
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2023-03-08 15:58:34 -0800

    Create Holiday.js

---
+++ Holiday.js
@@ -0,0 +1,14 @@
+function Thanksgiving(){
+      if (month == 10 && day ==  24 && year== 2022  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  23 && year== 2023  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  28 && year== 2024  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  27 && year== 2025  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  26 && year== 2026  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  25 && year== 2027  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  23 && year== 2028  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  22 && year== 2029  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  28 && year== 2030  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  27 && year== 2031  ){return " Happy Thanksgiving!";}
+     else if (month == 10 && day ==  25 && year== 2032  ){return " Happy Thanksgiving!";}
+    else {return  " "  ;}
+}
