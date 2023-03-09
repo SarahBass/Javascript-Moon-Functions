7ec940e13a9e8fb1a8edca13f444eea30e9d3950
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2023-03-08 16:07:37 -0800

    Update Holiday.js

--- Holiday.js
+++ Holiday.js
@@ -1,25 +1,25 @@
 function Holiday(){
       
       if (month == 10 && day ==  24 && year== 2022  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  23 && year== 2023  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  28 && year== 2024  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  27 && year== 2025  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  26 && year== 2026  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  25 && year== 2027  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  23 && year== 2028  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  22 && year== 2029  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  28 && year== 2030  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  27 && year== 2031  ){return " Happy Thanksgiving!";}
-      else if (month == 10 && day ==  25 && year== 2032  ){return " Happy Thanksgiving!";}
-      else if(month==        1  && day == 1   && year == 2022   ){return "Happy Lunar New Year!";}
-      else if(month==         0 && day ==  22 && year ==  2023  ){return "Happy Lunar New Year!";}
-      else if(month==         1 && day ==  10 && year ==  2024  ){return "Happy Lunar New Year!";}
-      else if(month==         0 && day ==  29 && year ==  2025  ){return "Happy Lunar New Year!";}
-      else if(month==         1 && day ==  17 && year ==  2026  ){return "Happy Lunar New Year!";}
-      else if(month==        1  && day == 7   && year == 2027   ){return "Happy Lunar New Year!";}
-      else if(month==         0 && day ==  26 && year ==  2028  ){return "Happy Lunar New Year!";}
-      else if(month==         1 && day ==  13 && year ==  2029  ){return "Happy Lunar New Year!";}
-      else if(month==        1  && day == 2   && year == 2030   ){return "Happy Lunar New Year!";}
+      else if (month == 10 && day ==  23 && year == 2023  ){return " Happy Thanksgiving!";}
+      else if (month == 10 && day ==  28 && year == 2024  ){return " Happy Thanksgiving!";}
+      else if (month == 10 && day ==  27 && year == 2025  ){return " Happy Thanksgiving!";}
+      else if (month == 10 && day ==  26 && year == 2026  ){return " Happy Thanksgiving!";}
+      else if (month == 10 && day ==  25 && year == 2027  ){return " Happy Thanksgiving!";}
+      else if (month == 10 && day ==  23 && year == 2028  ){return " Happy Thanksgiving!";}
+      else if (month == 10 && day ==  22 && year == 2029  ){return " Happy Thanksgiving!";}
+      else if (month == 10 && day ==  28 && year == 2030  ){return " Happy Thanksgiving!";}
+      else if (month == 10 && day ==  27 && year == 2031  ){return " Happy Thanksgiving!";}
+      else if (month == 10 && day ==  25 && year == 2032  ){return " Happy Thanksgiving!";}
+      else if (month==  1  && day ==   1 && year == 2022  ){return "Happy Lunar New Year!";}
+      else if (month==  0  && day ==  22 && year ==  2023 ){return "Happy Lunar New Year!";}
+      else if (month==  1  && day ==  10 && year ==  2024 ){return "Happy Lunar New Year!";}
+      else if (month==  0  && day ==  29 && year ==  2025 ){return "Happy Lunar New Year!";}
+      else if (month==  1  && day ==  17 && year ==  2026 ){return "Happy Lunar New Year!";}
+      else if (month==  1  && day == 7   && year ==  2027 ){return "Happy Lunar New Year!";}
+      else if (month==  0  && day ==  26 && year ==  2028 ){return "Happy Lunar New Year!";}
+      else if (month==  1  && day ==  13 && year ==  2029 ){return "Happy Lunar New Year!";}
+      else if (month==  1  && day == 2   && year ==  2030 ){return "Happy Lunar New Year!";}
  
     else {return  " "  ;}
 }
