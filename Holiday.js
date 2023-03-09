db5b69533a92dee4727dc900ffcd85891ec7f9d4
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2023-03-08 16:26:04 -0800

    Update Holiday.js

--- Holiday.js
+++ Holiday.js
@@ -39,7 +39,16 @@ function Holiday(){
                  else if(month == 6 && day == 4 ){return "Happy Fourth of July!";}
                  else if(month == 5 && day == 4 ){return "May the 4th be with you!";}
                  else if(month == 3 && day == 17 ){return "Happy St. Patrick's Day!";}
+                 else if      (  (year) == 2022 && month == 11 && ( day > 18 || day < 23)) {return "Happy Hanukkah!";}
+                 else if       (  (year) == 2023 && month == 11 && ( day > 7 || day < 15)) {return "Happy Hanukkah!";}
+                 else if      (  (year) == 2024 && month == 11 && ( day > 26 || day < 30)) {return "Happy Hanukkah!";}
+                 else if      (  (year) == 2025 && month == 11 && ( day > 14 || day < 22)) {return "Happy Hanukkah!";}
+                 else if       (  (year) == 2026 && month == 11 && ( day > 4 || day < 12)) {return "Happy Hanukkah!";}
+                 else if      (  (year) == 2027 && month == 11 && ( day > 26 || day < 30)) {return "Happy Hanukkah!";}
+                 else if      (  (year) == 2028 && month == 11 && ( day > 12 || day < 20)) {return "Happy Hanukkah!";}
+                 else if        (  (year) == 2029 && month == 11 && ( day > 1 || day < 9)) {return "Happy Hanukkah!";}
+                 else if      (  (year) == 2030 && month == 11 && ( day > 20 || day < 23)) {return "Happy Hanukkah!";}
       
  
-    else {return  " "  ;}
+    else {return  "Today is " + dayNames[week] + " , " + monthNames[month] + " " + day + " " + year  ;}
 }
