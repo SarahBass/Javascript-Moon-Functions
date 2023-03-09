2aaedab9231bb3f6c76bbefd465a4224d7c040d9
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2023-03-08 17:15:40 -0800

    Create Planet.js

---
+++ Planet.js
@@ -0,0 +1,68 @@
+                    if (year == 2022 && month ==  0 ){return  "Mercury Visible at Sunset until 30th";}
+                    else if (year == 2022 && month ==  1 ){return  "Venus Brightest on February 9";}
+                    else if (year == 2022 && month ==  2 ){return  "Spring Equinox on March 20th";}
+                    else if (year == 2022 && month ==  3 ){return  "Mercury will be visible at Sunrise until 30th";}
+                    else if (year == 2022 && month ==  4 ){return  "May 6: Eta Aquarid Meteors";}
+                    else if (year == 2022 && month ==  5 ){return  "June 20: Summer Solstice";}
+                    else if (year == 2022 && month ==  6 ){return  "Pluto at Opposition Jul 20";}
+                    else if (year == 2022 && month ==  7 ){return  "August 24 : Uranus begins retrograde motion";}
+                    else if (year == 2022 && month ==  8 ){return  "Fall Equinox September 22nd";}
+                    else if (year == 2022 && month ==  9 ){return  "Mars in Retrograde October 30";}
+                    else if (year == 2022 && month ==  10 ){return  "Uranus at opposition – November 9";}
+                    else if (year == 2022 && month ==  11 ){return  "Dec 21: Winter Solstice";}
+                    else if (year == 2023 && month ==  0 ){return  "Jan 7: Inferior conjunction Mercury";}
+                    else if (year == 2023 && month ==  1 ){return  "February 16: Saturn in conjunction with the sun";}
+                    else if (year == 2023 && month ==  2 ){return  "Spring Equinox on March 20th";}
+                    else if (year == 2023 && month ==  3 ){return  "April 11: Jupiter at solar conjunction";}
+                    else if (year == 2023 && month ==  4 ){return  "Mercury Visible at Sunrise until 30th";}
+                    else if (year == 2023 && month ==  5 ){return  "June 21: Summer Solstice";}
+                    else if (year == 2023 && month ==  6 ){return  "Pluto at Opposition : July 22 ";}
+                    else if (year == 2023 && month ==  7 ){return  "August 13th: Perseid Meteor Shower";}
+                    else if (year == 2023 && month ==  8 ){return  "Fall Equinox September 23rd";}
+                    else if (year == 2023 && month ==  9 ){return  "October 22 - Orionids Meteor Shower";}
+                    else if (year == 2023 && month ==  10 ){return  "Uranus at opposition – November 13";}
+                    else if (year == 2023 && month ==  11 ){return  "Winter Solstice on Dec 21st";}
+                    else if (year == 2024 && month ==  0 ){return  "January 4 - Quadrantids Meteor Shower";}
+                    else if (year == 2024 && month ==  1 ){return  "Centaurid Meteor Shower";}
+                    else if (year == 2024 && month ==  2 ){return  "Spring Equinox March 19th";}
+                    else if (year == 2024 && month ==  3 ){return  "April 8: Total Solar Eclipse parts of USA";}
+                    else if (year == 2024 && month ==  4 ){return  "May 7 - Eta Aquarids Meteor Shower";}
+                    else if (year == 2024 && month ==  5 ){return  "Summer Solstice Jun 20th";}
+                    else if (year == 2024 && month ==  6 ){return  "Mercury visible at Sunset until 30th";}
+                    else if (year == 2024 && month ==  7 ){return  "August 13 - Perseids Meteor Shower";}
+                    else if (year == 2024 && month ==  8 ){return  "Fall Equinox September 24";}
+                    else if (year == 2024 && month ==  9 ){return  "October 22 - Orionids Meteor Shower";}
+                    else if (year == 2024 && month ==  10 ){return  "Mercury visible at Sunset until 30th";}
+                    else if (year == 2024 && month ==  11 ){return  "Winter Solstice Dec 21st";}
+                    else if (year == 2025 && month ==  0 ){return  "Mars at Opposition on January 16th";}
+                    else if (year == 2025 && month ==  1 ){return  "Venus at greatest brightness: Feb 16";}
+                    else if (year == 2025 && month ==  2 ){return  "Spring Equinox March 20th";}
+                    else if (year == 2025 && month ==  3 ){return  "April 23rd - Lyrids Meteor Shower";}
+                    else if (year == 2025 && month ==  4 ){return  "May 7th - Eta Aquarids Meteor Shower";}
+                    else if (year == 2025 && month ==  5 ){return  "Summer Solstice June 20th";}
+                    else if (year == 2025 && month ==  6 ){return  "July 29th - Delta Aquarids Meteor Shower";}
+                    else if (year == 2025 && month ==  7 ){return  "August 13th - Perseids Meteor Shower";}
+                    else if (year == 2025 && month ==  8 ){return  "Fall Equinox: September 22nd";}
+                    else if (year == 2025 && month ==  9 ){return  "October 22nd - Orionids Meteor Shower";}
+                    else if (year == 2025 && month ==  10 ){return  "November 5th - Taurids Meteor Shower";}
+                    else if (year == 2025 && month ==  11 ){return  "Winter Solstice : December 21st";}
+                    else if (year == 2026 && month ==  2 ){return  "Spring Equinox March 20th";}
+                    else if (year == 2026 && month ==  5 ){return  "Summer Solstice June 21st";}
+                    else if (year == 2026 && month ==  8 ){return  "Fall Equinox: September 22nd";}
+                    else if (year == 2026 && month ==  11 ){return  "Winter Solstice : December 21st";}
+                    else if (year == 2027 && month ==  2 ){return  "Spring Equinox March 20th";}
+                    else if (year == 2027 && month ==  5 ){return  "Summer Solstice June 21st";}
+                    else if (year == 2027 && month ==  8 ){return  "Fall Equinox: September 22nd";}
+                    else if (year == 2027 && month ==  11 ){return  "Winter Solstice : December 21st";}
+                    else if (year == 2028 && month ==  2 ){return  "Spring Equinox March 19th";}
+                    else if (year == 2028 && month ==  5 ){return  "Summer Solstice June 20th";}
+                    else if (year == 2028 && month ==  8 ){return  "Fall Equinox: September 22nd";}
+                    else if (year == 2028 && month ==  11 ){return  "Winter Solstice : December 21st";}
+                    else if (year == 2029 && month ==  2 ){return  "Spring Equinox March 20th";}
+                    else if (year == 2029 && month ==  5 ){return  "Summer Solstice June 20th";}
+                    else if (year == 2029 && month ==  8 ){return  "Fall Equinox: September 22nd";}
+                    else if (year == 2029 && month ==  11 ){return  "Winter Solstice : December 21st";}
+                    else if (year == 2030 && month ==  2 ){return  "Spring Equinox March 20th";}
+                    else if (year == 2030 && month ==  5 ){return  "Summer Solstice June 21st";}
+                    else if (year == 2030 && month ==  8 ){return  "Fall Equinox: September 22nd";}
+                    else if (year == 2030 && month ==  11 ){return  "Winter Solstice : December 21st";}
